88808229809296e80a81fbf6c6fa774e8d6924bd
khanhdii-08 <phamduy2408@gmail.com>
2022-09-18 21:07:06 +0700

    tuan 3

---
+++ Tuan3/spring_boot_api_jwt/src/main/java/com/example/spring_boot_api_jwt/SpringBootApiJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_api_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiJwtApplication.class, args);
+	}
+
+}
